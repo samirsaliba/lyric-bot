1005e075026a65ea42906de45e0000cf4c918fe9
Samir <samirtsj@gmail.com>
2019-06-24 17:02:59 -0300

    Add existing file

--- bot.js
+++ bot.js
@@ -10,20 +10,80 @@ const Bot = new Twit({
 });
 
 function getRndInteger(min, max) {
+	/*
+	returns a round integer between min (INCLUDED) and max (EXCLUDED)
+	*/
   return Math.floor(Math.random() * (max - min) ) + min;
 }
 
 function pickRandomBar(lyrics) {
+	/*
+	splits "lyrics" which is the full text of the file into an array of substrings
+	the / char is used as separator
+	subs the first 2 chars so it deletes the blank line bewteen lyrics
+
+	then, randomizes an integer between 0 and the number of lyrics on the file
+	and uses this integer as index to the array of substrings
+	*/
 
     var aux = lyrics.split("/");
     const random =  getRndInteger(0, aux.length);
-    var ret = aux[random].substr(2, aux[random].length)
-    console.log("# of tweet candidates ")
-    console.log(aux.length)
-   	console.log("chosen tweet # ")
-    console.log(random)
-    console.log(ret)
+    var ret = aux[random].substr(2, aux[random].length);
+    console.log("# of tweet candidates ");
+    console.log(aux.length);
+   	console.log("chosen tweet # ");
+    console.log(random);
+    console.log(ret);
     return ret;
+}
+
+function gnol(file){
+	/*
+	gnol as get-number-of-lyrics
+	prints to console the number of lyrics the album passed as parameter has
+	*/
+	fs.readFile(file, 'utf8', function(error, text) {  
+        if (error) {
+            console.log(error.message);
+        } else {
+            var aux = text.split("/");
+            console.log(file + " | " + aux.length);
+        }
+    });
+}
+
+function getNumberOfLyrics(){
+	/*
+	for each album calls gnol, which in turn prints to console the number of lyrics of the album passed
+	*/
+	console.log("file  |  #Lyrics");
+
+	file = "lyrics/tcdo.txt";
+	gnol(file);
+
+	file = "lyrics/lr.txt";
+	gnol(file);
+
+	file = "lyrics/grad.txt";
+	gnol(file);
+
+	file = "lyrics/808s.txt";
+	gnol(file);
+
+	file = "lyrics/mbdtf.txt";
+	gnol(file);
+
+	file = "lyrics/yeezus.txt";
+	gnol(file);
+
+	file = "lyrics/tlop.txt";
+	gnol(file);
+
+	file = "lyrics/ye.txt";
+	gnol(file);
+
+	file = "lyrics/ksg.txt";
+	gnol(file);
 }
 
 function pickRandomFile(){
@@ -31,9 +91,17 @@ function pickRandomFile(){
 	The approach below forces the probability of each tweet candidate
 	to be the same, considering the lyrics (tweet candidates) 
 	count for each album at the time;
-	If any more lyrics are added to one or more of the files,
-	then the if conditions must be updated
+
+	PS. If any lyrics are added or removed, the code should be updated so the probabilities are preserved; 
+		If not, the bot will continue to function normally but some lyrics will be a bit more likely to be tweeted than others;
+		To get the numbers of lyrics per file, simply call getNumberOfLyrics and it'll be printed to console.
+
+		getNumberOfLyrics(); 
 	*/
+
+	
+	// if you wanna check if the probabilities are correct, uncomment all the lines starting with '//'
+	// it will pick the files 100.000 times and print to console how many times each of the files was chosen.
 
 	//var i=0;
 	//var tcdo=0, lr=0, grad=0, eights=0, mbdtf=0, yeezus=0, tlop=0, ye=0, ksg=0; 
@@ -88,17 +156,16 @@ function pickRandomFile(){
 		//i++;
 	//}
 
-	/*
-	console.log("tcdo = " +tcdo);
-	console.log("lr = " +lr);
-	console.log("grad = " +grad);
-	console.log("eights = " +eights);
-	console.log("mbdtf = " +mbdtf);
-	console.log("yeezus = " +yeezus);
-	console.log("tlop = " +tlop);
-	console.log("ye = " +ye);
-	console.log("ksg = " +ksg);
-	*/
+	// console.log("tcdo = " +tcdo);
+	// console.log("lr = " +lr);
+	// console.log("grad = " +grad);
+	// console.log("eights = " +eights);
+	// console.log("mbdtf = " +mbdtf);
+	// console.log("yeezus = " +yeezus);
+	// console.log("tlop = " +tlop);
+	// console.log("ye = " +ye);
+	// console.log("ksg = " +ksg);
+	
 	/*
 	//The approach below selects one album randomly,
 	//each album having the same probability of being selected;
@@ -108,46 +175,45 @@ function pickRandomFile(){
 
 	random =  getRndInteger(0, 9);
 	switch (random) {
-  	case 0:
-    	file = "lyrics/tcdo.txt";
-    break;
+	  	case 0:
+	    	file = "lyrics/tcdo.txt";
+	    break;
 
-  	case 1:
-    	file = "lyrics/lr.txt";
-    break;
+	  	case 1:
+	    	file = "lyrics/lr.txt";
+	    break;
 
-  	case 2:
-     	file = "lyrics/grad.txt";
-   	break;
+	  	case 2:
+	     	file = "lyrics/grad.txt";
+	   	break;
 
-  	case 3:
-    	file = "lyrics/808s.txt";
-    break;
+	  	case 3:
+	    	file = "lyrics/808s.txt";
+	    break;
 
-  	case 4:
-    	file = "lyrics/mbdtf.txt";
-    break;
+	  	case 4:
+	    	file = "lyrics/mbdtf.txt";
+	    break;
 
-  	case 5:
-    	file = "lyrics/yeezus.txt";
-    break;
+	  	case 5:
+	    	file = "lyrics/yeezus.txt";
+	    break;
 
-  	case 6:
-    	file = "lyrics/tlop.txt";
-    break;
+	  	case 6:
+	    	file = "lyrics/tlop.txt";
+	    break;
 
-   	case 7:
-   		file = "lyrics/ye.txt";
-   	break;
+	   	case 7:
+	   		file = "lyrics/ye.txt";
+	   	break;
 
-   	case 8:
-   		file = "lyrics/ksg.txt";	
-   	break;
-}
+	   	case 8:
+	   		file = "lyrics/ksg.txt";	
+	   	break;
+	}
 	*/
 
 	return file;
-
 }
 
 function tweet() {
@@ -164,11 +230,19 @@ function tweet() {
             let tweet = bar;
             console.log(tweet)
             
+            // the code below actually (tries to) tweet, so if you're testing the bot 
+            // make sure to comment the following lines
+
+            /*
             Bot.post('statuses/update', {status: tweet}, function(error, tweet, response) {
                 if (error) {
                     console.log("Error making post. ", error.message);
                 };
+                else {
+					console.log("Tweet sent!.");
+                }
             });
+            */
         }
     });
 }
